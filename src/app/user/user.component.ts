98d637d64551648ca741d7ad7607afe6766422bd
aqamine <aqebliamine1@gmail.com>
2025-09-22 03:06:38 +0200

    init

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,41 @@
+import {
+  Component,
+  computed,
+  EventEmitter,
+  input,
+  Input,
+  output,
+  Output,
+} from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
+import { type User } from './user.model';
+
+
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css',
+})
+export class UserComponent {
+  @Input({ required: true }) user!: User;
+  @Input({required: true}) selected!: boolean;
+  @Output() select = new EventEmitter<string>();
+
+  // select = output<string>();
+
+  // avatar = input.required<string>();
+  // name = input.required<string>();
+
+  // imagePath = computed(() => {
+  //   return 'assets/users/' + this.avatar();
+  // });
+
+  get imagePath() {
+    return 'assets/users/' + this.user.avatar;
+  }
+  onSelectUser() {
+    this.select.emit(this.user.id);
+  }
+}
