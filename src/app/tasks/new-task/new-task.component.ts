b67c6adbfc8d040b53261a5ee135ffcdbd2ce7fc
aqamine <aqebliamine1@gmail.com>
2025-10-02 23:51:19 +0200

    use servises and injection

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,6 +1,7 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { type NewTaskData } from '../task/task.model';
+import { TasksService } from '../tasks.service';
 
 @Component({
   selector: 'app-new-task',
@@ -10,23 +11,26 @@ import { type NewTaskData } from '../task/task.model';
   styleUrl: './new-task.component.css'
 })
 export class NewTaskComponent {
-  @Output() cancel = new EventEmitter<void>();
-  @Output() add = new EventEmitter<NewTaskData>();
+  @Input({required: true}) userId!: string;
+  @Output() close = new EventEmitter<void>();
+
 
   entredTitle = '';
   entredSummary = '';
   entredDate = '';
+  private taskService = inject(TasksService);
 
   onCancel() {
-    this.cancel.emit();
+    this.close.emit();
   }
 
   onSubmit() {
-    this.add.emit({
+    this.taskService.addTask({
       title: this.entredTitle,
       summary: this.entredSummary,
       date: this.entredDate,
-    });
+    }, this.userId);
+    this.close.emit();
   }
   
 }
